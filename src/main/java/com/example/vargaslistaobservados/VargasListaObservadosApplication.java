39577f824955257604d3e41a3c22cee860a49224
Ozymandias-II <jordy_alexander_vargas@outlook.com>
2022-07-13 19:46:31 -0500

    primer commit

---
+++ src/main/java/com/example/vargaslistaobservados/VargasListaObservadosApplication.java
@@ -0,0 +1,13 @@
+package com.example.vargaslistaobservados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VargasListaObservadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VargasListaObservadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vargaslistaobservados/VargasListaObservadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vargaslistaobservados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VargasListaObservadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
